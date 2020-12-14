0a695019101ead886ae17e0efa152cf10a85b24a
Gabriel-britto <71145717+Gabriel-britto@users.noreply.github.com>
2020-12-14 18:58:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+function myFunction() {
+  var x = document.getElementById("info");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("info2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("info3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("info4");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction5() {
+  var x = document.getElementById("info5");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction6() {
+  var x = document.getElementById("info6");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction7() {
+  var x = document.getElementById("info7");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction8() {
+  var x = document.getElementById("info8");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } 
+  else {
+    x.style.display = "block";
+  }
+}
